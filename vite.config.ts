b8bce0076f7a84f644b15b8317a95612189a0dc9
thisLight <l1589002388@gmail.com>
2022-10-09 15:57:31 +0800

    vite: remove plugin workaround

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
     plugins: [suidPlugin(), solidPlugin(), splitVendorChunkPlugin()],
-    optimizeDeps: {
-        exclude: ["@suid/material", "@suid/icons-material"], // workaround to vite-plugin-solid@2.3.3,2.3.5,2.3.6 breaking change
-    },
     server: {
         port: 3000,
     },
