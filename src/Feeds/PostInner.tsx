58878e0070ab15b50fa6194f68cf2f7fcf6d9c93
thisLight <l1589002388@gmail.com>
2022-11-11 08:43:22 +0800

    PostInner: minor cleanup

--- src/Feeds/PostInner.tsx
+++ src/Feeds/PostInner.tsx
@@ -54,13 +54,8 @@ const PostInner: Component<PostInnerProps> = (props) => {
             ? {
                   boxShadow:
                       "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
-                  transition:
-                      "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
               }
-            : {
-                  transition:
-                      "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-              };
+            : {};
     return (
         <Paper
             sx={{
@@ -78,6 +73,8 @@ const PostInner: Component<PostInnerProps> = (props) => {
             <Toolbar
                 sx={{
                     display: "flex",
+                    transition:
+                        "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
                     ...toolbarShadowSx(),
                 }}
             >
